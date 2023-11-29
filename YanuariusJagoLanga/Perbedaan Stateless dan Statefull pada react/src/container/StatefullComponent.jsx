dd8fe2fdfda34322843d4642285e94ebc3f85367
NorrysLanga <langanoris@gmail.com>
2023-11-29 15:16:58 +0700

    update data

---
+++ YanuariusJagoLanga/Perbedaan Stateless dan Statefull pada react/src/container/StatefullComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+class StatefullComponent extends React.Component {
+    render(){
+      return <p>statefull Component</p>
+    }
+  }
+  
+  export default StatefullComponent;
